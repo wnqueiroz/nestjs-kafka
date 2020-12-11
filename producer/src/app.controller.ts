27e26ac0fca26b94c84610ecc35a5f8fc282f5e9
William Queiroz <wnqueiroz@outlook.com>
2020-12-11 17:43:16 -0300

    feat: created Kafka producer

--- producer/src/app.controller.ts
+++ producer/src/app.controller.ts
@@ -1,12 +1,45 @@
 import { Controller, Get } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
+
 import { AppService } from './app.service';
+
+const kafkaHost = process.env.KAFKA_HOST || 'localhost';
+const kafkaPort = process.env.KAFKA_PORT || '9093';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'kafkaSample',
+        brokers: [`${kafkaHost}:${kafkaPort}`],
+      },
+      consumer: {
+        groupId: 'my-kafka-consumer', // Should be the same thing we give in consumer
+      },
+    },
+  })
+  client: ClientKafka;
+
+  async onModuleInit(): Promise<void> {
+    // Need to subscribe to topic
+    // so that we can get the response from kafka microservice
+    this.client.subscribeToResponseOf('my-first-topic');
+
+    await this.client.connect();
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<any> {
+    const consumerResponse = await this.client
+      .send('my-first-topic', 'Hello Kafka')
+      .toPromise(); // args - topic, message
+
+    return {
+      consumerResponse,
+    };
   }
 }
--- producer/src/main.ts
+++ producer/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT || 80;
+
+  await app.listen(port, () =>
+    console.log('Kafka producer service is listening!'),
+  );
 }
 bootstrap();
